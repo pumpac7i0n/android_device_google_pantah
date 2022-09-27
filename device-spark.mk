71e1952bde23d076422b4ddcd46e6282e6671a82
Matt Filetto <matt.filetto@gmail.com>
2022-09-27 02:07:47 -0700

    rro_overlay: enable components for ambient music detection to work
    * These are disabled globally and enabled on a device by device basis
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    Change-Id: I49aa06c7cb928b4c0f953c22cf28de2222c54adc
    
    # Conflicts:
    #	device-lineage.mk
    
    # Conflicts:
    #	device-lineage.mk
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-spark.mk
+++ device-spark.mk
@@ -4,14 +4,15 @@ $(call inherit-product, device/google/pantah/artifacts.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/google/pantah/overlay-spark
 
+# RRO Overlays
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay \
+    NowPlayingOverlay
+
 # Camera
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
-
-# EUICC
-PRODUCT_PACKAGES += \
-    EuiccSupportPixelOverlay
 
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
