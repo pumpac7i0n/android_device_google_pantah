0be7aa0bdb9beb4f6ef45c8858eeac4a7eaaf770
Matt Filetto <matt.filetto@gmail.com>
2022-09-27 01:49:30 -0700

    overlay: Settings: Enable Smooth Display setting
    All GS101 devices (raven and oriole) have peak refresh rates higher than
    60 Hz, so allow users to toggle high-refresh rate like on stock.
    
    Change-Id: I7fcbb905b61575de8c5e9d9a935463932551fb6a
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>
    
    # Conflicts:
    #	device-lineage.mk
    
    # Conflicts:
    #	device-spark.mk
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-spark.mk
+++ device-spark.mk
@@ -1,2 +1,11 @@
 # artifacts
 $(call inherit-product, device/google/pantah/artifacts.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/pantah/overlay-spark
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
