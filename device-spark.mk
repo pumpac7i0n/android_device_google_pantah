74652330de1abe407183dab66866797d26e86a20
Matt Filetto <matt.filetto@gmail.com>
2022-09-27 02:21:40 -0700

    pantah: add build property for device maintainer username
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-spark.mk
+++ device-spark.mk
@@ -16,11 +16,16 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.service.debuggable=1 \
     persist.service.adb.enable=1
 
-# product properties
+# properties - product
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
+# properties - system
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.spark.maintainer=Albinoman887
+    ro.spark.maintainer.username=Albinoman887
 
 # Quick Tap
 TARGET_SUPPORTS_QUICK_TAP := true
